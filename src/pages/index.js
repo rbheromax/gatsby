403ab63794d774d040b75d37229552d5ef8f4ab9
Tezz <rbheormax@gmail.com>
2020-11-15 16:14:22 -0500

    Update Contact Form

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Header from '@sections/Header';
 import About from '@sections/About';
 import Brands from '@sections/Brands';
 import Products from '@sections/Products';
-import Contact from '@sections/Contact';
+import ContactForm from '@sections/Contact';
 import Team from '@sections/Team';
 import Faq from '@sections/Faq';
 import Footer from '@sections/Footer';
@@ -20,6 +20,7 @@ const IndexPage = () => (
     <Products />
     <Team />
     <Faq />
+    <ContactForm />
     <Footer />
   </Layout>
 );
